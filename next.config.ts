e99f895aa57ac8832a7fad759edb5042384c465a
Wolverh <104695571+Wolverh@users.noreply.github.com>
2026-07-01 23:08:44 +0300

    fix: bypass typescript and eslint errors during build

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
